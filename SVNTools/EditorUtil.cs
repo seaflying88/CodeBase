3b641a5f436e9ea114e897f6c935d6d592328233
seaflying88 <seaflying@live.cn>
2018-04-13 09:56:28 +0800

    Add files via upload

---
+++ SVNTools/EditorUtil.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Debug = UnityEngine.Debug;
+
+namespace Assets.Editor
+{
+    public class EditorUtil
+    {
+        public static void ExecuteProcess(string filePath, string command, string workPath = "", int seconds = 0)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                return;
+            }
+            Process process = new Process();//创建进程对象
+            process.StartInfo.WorkingDirectory = workPath;
+            process.StartInfo.FileName = filePath;
+            process.StartInfo.Arguments = command;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardOutput = false;//不重定向输出
+            try
+            {
+                if (process.Start())
+                {
+                    if (seconds == 0)
+                    {
+                        process.WaitForExit(); //无限等待进程结束
+                    }
+                    else
+                    {
+                        process.WaitForExit(seconds); //等待毫秒
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+            }
+            finally
+            {
+                process.Close();
+            }
+        }
+    }
+}
---
+++ SVNTools/SvnUtil.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+
+namespace Assets.Editor.Svn
+{
+    public class SvnUtil : UnityEditor.Editor
+    {
+        private static string _svnPath = "";
+
+        public static void DelAndUpdateSvnFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            ExecuteProcess(GetSvnProcPath(), string.Format("/command:update /path:{0} /closeonend:2", path));
+        }
+
+        public static void RevertAndUpdateSvnDirectory(string path)
+        {
+            ExecuteProcess(GetSvnProcPath(), string.Format("/command:revert -r /path:{0} /closeonend:2", path));
+            ExecuteProcess(GetSvnProcPath(), string.Format("/command:update /path:{0} /closeonend:2", path));
+        }
+
+        public static void UpdateSvnDirectory(string path)
+        {
+            ExecuteProcess(GetSvnProcPath(), string.Format("/command:update /path:{0} /closeonend:2", path));
+        }
+
+        public static void CommitSvnDirectory(string path)
+        {
+            ExecuteProcess(GetSvnProcPath(), string.Format("/command:commit /path:{0} /closeonend:0", path));
+        }
+
+        public static void AddSvnDirectory(string path)
+        {
+            ExecuteProcess(GetSvnProcPath(), string.Format("/command:add /path:{0} /closeonend:0", path));
+        }
+
+        public static void ProcessSvnCommand(string command)
+        {
+            ExecuteProcess(GetSvnProcPath(), command);
+        }
+
+        private static List<string> drives = new List<string>() { "c:", "d:", "e:", "f:" };
+
+        private static string svnPath = @"\Program Files\TortoiseSVN\bin\";
+        private static string svnProc = @"TortoiseProc.exe";
+        private static string svnProcPath = "";
+
+        private static string GetSvnProcPath()
+        {
+            if (_svnPath != string.Empty)
+            {
+                return _svnPath;
+            }
+            foreach (string item in drives)
+            {
+                string path = string.Concat(item, svnPath, svnProc);
+                if (File.Exists(path))
+                {
+                    _svnPath = path;
+                    break;
+                }
+            }
+            if (_svnPath == string.Empty)
+            {
+                _svnPath = EditorUtility.OpenFilePanel("Select TortoiseProc.exe", "c:\\", "exe");
+            }
+            //可将路径存到本地注册表
+            return _svnPath;
+        }
+
+        private static void ExecuteProcess(string filePath, string command, int seconds = 0)
+        {
+            EditorUtil.ExecuteProcess(filePath, command, "", seconds);//参见文末另一篇：C#调用可执行文件
+        }
+    }
+}
